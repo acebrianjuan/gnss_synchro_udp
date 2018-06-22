d713d59cacbfdae0979d816fc281edbb3d8de57f
Álvaro Cebrián Juan <acebrianjuan@gmail.com>
2018-06-22 17:22:52 +0200

    Remove blank lines

--- test_write.cc
+++ test_write.cc
@@ -72,9 +72,7 @@ int main(int argc, char* argv[])
       if(i % 4 == 0)
       {
     	  udp_sink.write_gnss_synchro(stocks);
-
     	  stocks.clear();
-
     	  id = 0;
 
     	  std::this_thread::sleep_for(std::chrono::milliseconds(1));
